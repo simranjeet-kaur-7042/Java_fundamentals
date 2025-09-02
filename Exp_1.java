52334a24501d21b8d541138c55cf14df787f69c0
simranjeet-kaur-7042 <simranjeet.kaur.4415@gmail.com>
2025-09-02 11:03:23 +0530

    initial commit

---
+++ Exp_1.java
@@ -0,0 +1,30 @@
+public class Exp_1{
+    public static void main(String[] args) {
+        // Primitive data types in Java
+        byte byteVar = 100;
+        
+        short shortVar = 10000;
+
+        int intVar = 100000;
+        
+        long longVar = 10000000000L;
+        
+        float floatVar = 10.5f;
+        
+        double doubleVar = 20.99;
+        
+        char charVar = 'A';
+        
+        boolean booleanVar = true;
+
+        // Printing values
+        System.out.println("byte value: " + byteVar);
+        System.out.println("short value: " + shortVar);
+        System.out.println("int value: " + intVar);
+        System.out.println("long value: " + longVar);
+        System.out.println("float value: " + floatVar);
+        System.out.println("double value: " + doubleVar);
+        System.out.println("char value: " + charVar);
+        System.out.println("boolean value: " + booleanVar);
+    }
+}
